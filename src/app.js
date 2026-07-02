d995e69d5882f0e66775648eb2a0e30ca395daff
Md Danyal Haider <danyalhaider068@gmail.com>
2026-07-02 16:02:08 +0530

    Understanding routing methods

--- src/app.js
+++ src/app.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
 
+// app.use("/", (req, res) => {
+//     res.send("Namaste");
+// })
+
+
+// Will work for all type of http routes
 app.use("/test", (req, res) => {
     res.send("Hello from the server!");
 });
-app.use("/hello", (req, res) => {
-    res.send("hello, hello");
+
+app.get("/user", (req,res) => {
+    res.send({ first_name: "Danyal", last_name: "Haider" });
 });
 
-app.use("", (req, res) => {
-    res.send("Namaste");
+app.post("/user",(req,res)=>{
+    res.send("Data is saved on the database");
+})
+
+app.put("/user",(req,res)=>{
+    res.send("User's data is updated.");
 });
+app.delete("/user",(req,res)=>{
+    res.send("User's data is deleted");
+})
+// app.use("/hello", (req, res) => {
+//     res.send("hello, hello");
+// });
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000 ")
 });
